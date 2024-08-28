b9e5bd4e840947b5eae1b28d732ed12601f53a95
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-28 18:28:58 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h1>Mi Portfolio</h1>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+  </nav>
+);
+
+export default Navbar;
 
