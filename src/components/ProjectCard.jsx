cfef3ec5b7157590e913023a6e5a07b6856ca9cd
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-28 17:48:24 +0200

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,1 +1,14 @@
 
+import styles from "../styles/ProjectCard.module.css";
+
+export default function ProjectCard({ project }) {
+  return (
+    <div className={styles.card_container}>
+      <h3>{project.title}</h3>
+      <p>{project.description}</p>
+      <a href={project.pdf} target="_blank" rel="noopener noreferrer">
+        Ver PDF
+      </a>
+    </div>
+  );
+}
