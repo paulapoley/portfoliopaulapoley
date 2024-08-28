33bd603bdd953a78ede626503ba7211d0f148e51
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 00:31:41 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css';
 const Navbar = () => (
   <nav className="navbar">
     <div className="navbar-links">
-      <a href="/">Home</a>
+      <a href="/">Proyectos</a>
       <a href="/about">Sobre mi</a>
     </div>
   </nav>
