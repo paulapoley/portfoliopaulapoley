b94987065dc8c54519e7485d5fe2c5c1fd515a0c
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 00:03:39 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,22 +4,25 @@ import './About.css';
 const About = () => {
   return (
     <div className="about-container">
-      <div className="about-content">
-        <h1 className="about-title">Sobre mí</h1>
-        <p className="about-text">
-          Soy Paula Poley, estudiante de último curso de <span className="highlight">Ingeniería de la Salud</span> con especialización en <span className="highlight">Informática Clínica</span> por la <span className="highlight">Universidad de Sevilla</span>. Durante mi formación he manejado programación (Java, Python, SQL), gestión de bases de datos, inteligencia artificial (TensorFlow, Keras), herramientas de desarrollo (Visual Studio Code, Docker), sistemas operativos, máquinas virtuales y herramientas de diseño.
-        </p>
-      </div>
-      <div className="about-links-container">
-        <p className="about-links-intro">
-          Check out my profiles:
-        </p>
-        <div className="about-links">
-          <a href="https://www.linkedin.com/in/paulapoley/" className="about-link linkedin" target="_blank" rel="noopener noreferrer">
-            LinkedIn
-          </a>
-          <a href="https://github.com/paulapoley" className="about-link github" target="_blank" rel="noopener noreferrer">
-            GitHub
+  <div className="about-content">
+    <h1 className="about-title">Sobre mí</h1>
+    <p className="about-intro">
+      Soy Paula Poley, estudiante de último curso de <span className="highlight">Ingeniería de la Salud</span> con especialización en <span className="highlight">Informática Clínica</span> por la <span className="highlight">Universidad de Sevilla</span>.
+    </p>
+    <p className="about-details">
+      Durante mi formación he manejado programación (Java, Python, SQL), gestión de bases de datos, inteligencia artificial (TensorFlow, Keras), herramientas de desarrollo (Visual Studio Code, Docker), sistemas operativos, máquinas virtuales y herramientas de diseño.
+    </p>
+  </div>
+  <div className="about-links-container">
+    <p className="about-links-intro">
+      Check out my profiles:
+    </p>
+    <div className="about-links">
+      <a href="https://www.linkedin.com/in/paulapoley/" className="about-link linkedin" target="_blank" rel="noopener noreferrer">
+        LinkedIn
+      </a>
+      <a href="https://github.com/paulapoley" className="about-link github" target="_blank" rel="noopener noreferrer">
+        GitHub
           </a>
         </div>
       </div>
