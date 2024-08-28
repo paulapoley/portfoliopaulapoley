bfe7a4ca423834edd7c42b3c69e5868bbc5dfde3
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-28 23:30:46 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import './About.css';
 
 const About = () => {
   return (
-    <div className="about-container">
-      <div className="about-content">
-        <h1 className="about-title">Sobre mí</h1>
-        <p className="about-text">
-          <span className="highlight"> Soy Paula Poley Ceballos, Ingeniería de la Salud con especialización en Informática Clínica por la Universidad de Sevilla.
-        </p>
-      </div>
+    <div>
+      <p className="about-text">
+        <span className="highlight">Soy Paula Poley Ceballos, Ingeniería de la Salud con especialización en Informática Clínica por la Universidad de Sevilla.</span>
+      </p>
       <div className="about-links-container">
         <p className="about-links-intro">
-          Conéctate conmigo:
+          {/* Contenido adicional */}
         </p>
-        <div className="about-links">
-          <a href="https://www.linkedin.com/in/paulapoley/" className="about-link linkedin" target="_blank" rel="noopener noreferrer">
-            LinkedIn
-          </a>
-          <a href="https://github.com/paulapoley" className="about-link github" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-        </div>
       </div>
     </div>
   );
