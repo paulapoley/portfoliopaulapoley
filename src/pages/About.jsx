50c123a37edc9e7ea235dd8a5b37d46701afea4d
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-28 18:30:15 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => (
+  <div className="about">
+    <h2>Sobre Mí</h2>
+    <p>Aquí puedes incluir información sobre ti, tu experiencia, y lo que te llevó a crear este portfolio.</p>
+  </div>
+);
+
+export default About;
 
