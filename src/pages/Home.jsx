b26d71ed90e3ecff784c9ffcf821c14d9e317b3e
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 01:03:29 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import './Home.css';
 
 const Home = () => {
   const projects = [
-    { id: 1, imgSrc: '/pdfs/p1.jpg', pdfSrc: '/pdfs/proyecto1.pdf' },
-    { id: 2, imgSrc: '/pdfs/p2.jpg', pdfSrc: '/pdfs/proyecto2.pdf' },
+    { id: 1, title: 'Proyecto 1', description: 'Descripción breve del proyecto 1.', imgSrc: '/pdfs/p1.jpg', pdfSrc: '/pdfs/proyecto1.pdf' },
+    { id: 2, title: 'Proyecto 2', description: 'Descripción breve del proyecto 2.', imgSrc: '/pdfs/p2.jpg', pdfSrc: '/pdfs/proyecto2.pdf' },
   ];
 
   const handleOpenPdf = (pdfSrc) => {
@@ -20,6 +20,10 @@ const Home = () => {
       <div className="projects-grid">
         {projects.map((project) => (
           <div className="project-card" key={project.id}>
+            <div className="project-info">
+              <h2 className="project-title">{project.title}</h2>
+              <p className="project-description">{project.description}</p>
+            </div>
             <img src={project.imgSrc} alt={`Proyecto ${project.id}`} className="project-image" />
             <button className="home-button" onClick={() => handleOpenPdf(project.pdfSrc)}>
               Ver PDF
