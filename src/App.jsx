962eecae49145e6a0daef5c968e1fe3a4237d6a3
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-28 18:28:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
