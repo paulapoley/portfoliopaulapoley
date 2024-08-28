2c88becfdc46cf019626371c859dfb3e54e9985b
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-28 18:43:10 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,29 +3,22 @@ import ProjectCard from '../components/ProjectCard';
 import './Home.css';
 
 const projects = [
-  {
-    title: 'Proyecto 1',
-    description: 'Descripción del Proyecto 1',
-    pdfUrl: '/projects/project1.pdf'
-  },
-  {
-    title: 'Proyecto 2',
-    description: 'Descripción del Proyecto 2',
-    pdfUrl: '/projects/project2.pdf'
-  },
-  // Agrega más proyectos aquí
+  { title: 'Proyecto 1', description: 'Descripción del Proyecto 1', pdf: 'proyecto1.pdf' },
+  { title: 'Proyecto 2', description: 'Descripción del Proyecto 2', pdf: 'proyecto2.pdf' },
+  // Añade más proyectos aquí
 ];
 
-const Home = () => (
-  <div className="home">
-    <h2>Mis Proyectos</h2>
-    <div className="projects-list">
-      {projects.map((project, index) => (
-        <ProjectCard key={index} project={project} />
-      ))}
+export default function Home() {
+  return (
+    <div>
+      <h2>Mis Proyectos</h2>
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
-export default Home;
 
