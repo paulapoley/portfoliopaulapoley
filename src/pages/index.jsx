7b9733aae7986e18f24b90e7e08701dd1ab90d84
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-28 17:47:58 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,17 @@
 
+import ProjectCard from "../components/ProjectCard";
+import projects from "../../projects.json";
+import styles from "../styles/globals.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Mis Proyectos de la Universidad</h1>
+      <div className={styles.project_gallery}>
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
+        ))}
+      </div>
+    </div>
+  );
+}
