8ca25d76f3c37dd1f12a82cf95adbab999b59bf3
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 15:08:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Certificaciones from './pages/Certificaciones';
+import './Layout.css'; 
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      <div className="content">
-        <Routes> {/* Cambia Switch por Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/sobre-mi" element={<About />} />
-          <Route path="/certificaciones" element={<Certificaciones />} />
-        </Routes>
+      <div className="main-container"> 
+        <Navbar /> 
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre-mi" element={<About />} />
+            <Route path="/certificaciones" element={<Certificaciones />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
 };
-
 export default App;
