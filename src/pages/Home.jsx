a47b083b278360dc32c9f5930a8d4e07ebfdcf4d
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 23:34:51 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
   const projects = [
     { 
       id: 1, 
-      title: 'Influencia Variables Exógenas en Detección de Enfermedades de Piel - Redes Neuronales', 
-      description: 'Análisis de cómo las variables externas afectan la detección de enfermedades usando redes neuronales.', 
+      title: 'Influencia de Variables Exógenas en la Detección de Enfermedades de Piel mediante Redes Neuronales', 
+      description: 'Análisis de cómo las variables externas (ubicación de la lesión, edad, sexo y tipo de diagnóstico) afectan la precisión de modelos de CNN en la detección de enfermedades cutáneas', 
       imgSrc: '/pdfs/TFG-PAULAPOLEY.jpg', 
       pdfSrc: '/pdfs/TFG-PAULAPOLEY.pdf' 
     },
