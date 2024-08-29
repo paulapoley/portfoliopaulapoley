11b2e2b8a02dab6035b65033fa49fcceb1e67051
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 12:08:03 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import favicon from '../favicon.ico';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={favicon} alt="Favicon" /> {/* Mostrar el favicon */}
+      </div>
       <ul className="navbar-menu">
-        <li><Link to="/">Proyectos</Link></li> 
-        <li><Link to="/about">Sobre mí</Link></li> 
-        <li><Link to="/certificaciones">Certificaciones</Link></li> 
+        <li><Link to="/">Proyectos</Link></li>
+        <li><Link to="/sobre-mi">Sobre mí</Link></li>
+        <li><Link to="/certificaciones">Certificaciones</Link></li>
       </ul>
     </nav>
   );
 };
+
 
 export default Navbar;
