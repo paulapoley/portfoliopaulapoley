0e597023843fd98ec5d48fa8644f8bbb59c93fa9
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 13:26:56 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,6 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <img src="/public/pdfs/icon.png" alt="Logo" /> {/* Mostrar el icon.png */}
-      </div>
       <ul className="navbar-menu">
         <li><Link to="/">Proyectos</Link></li>
         <li><Link to="/sobre-mi">Sobre mí</Link></li>
