3c0e91e4305bfe85e827de53e58c816ac26b0de7
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 18:39:37 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
     { id: 4, title: 'Proyecto 1', description: 'Descripción breve del proyecto 1.', imgSrc: '/pdfs/p1.jpg', pdfSrc: '/pdfs/proyecto1.pdf' },
     { id: 5, title: 'Proyecto 2', description: 'Descripción breve del proyecto 2.', imgSrc: '/pdfs/p2.jpg', pdfSrc: '/pdfs/proyecto2.pdf' },
     { id: 6, title: 'Proyecto 3', description: 'Descripción breve del proyecto 3.', imgSrc: '/pdfs/p3.jpg', pdfSrc: '/pdfs/proyecto3.pdf' },
-    { id: 7, title: 'Proyecto 1', description: 'Descripción breve del proyecto 1.', imgSrc: '/pdfs/p1.jpg', pdfSrc: '/pdfs/proyecto1.pdf' },
-    { id: 8, title: 'Digitalización de la autenticación y el control de acceso para servicio de salud de comunidad autónoma', description: 'Descripción breve del proyecto 2.', imgSrc: '/pdfs/p2.jpg', pdfSrc: '/pdfs/proyecto2.pdf' },
-    { id: 9, title: 'Análisis de datos con Weka', description: 'Se utiliza Weka para preprocesar y analizar datos del conjunto grupo_22.arff. Se aplican técnicas de clasificación, clustering y asociación para extraer patrones y conocimientos valiosos. El informe documenta la aplicación de filtros, algoritmos de aprendizaje automático y visualización de datos.', imgSrc: '/pdfs/p1.jpg', pdfSrc: '/pdfs/proyecto1.pdf' },
+    { id: 7, title: 'BYODSEC: Implementación de BYOD en una Entidad Hospitalaria con VPN Road Warrior TLS', imgSrc: '/pdfs/p1.jpg', pdfSrc: '/pdfs/proyecto1.pdf' },
+    { id: 8, title: 'Digitalización de la autenticación y el control de acceso para servicio de salud de comunidad autónoma', imgSrc: '/pdfs/p2.jpg', pdfSrc: '/pdfs/proyecto2.pdf' },
+    { id: 9, title: 'Análisis de datos con Weka', imgSrc: '/pdfs/p1.jpg', pdfSrc: '/pdfs/proyecto1.pdf' },
   ];
 
   const handleOpenPdf = (pdfSrc) => {
