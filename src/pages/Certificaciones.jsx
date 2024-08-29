1f0533032463ec3bd63e1f032934dd112ef8860d
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 14:59:38 +0200

    Update Certificaciones.jsx

--- src/pages/Certificaciones.jsx
+++ src/pages/Certificaciones.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import './Certificaciones.css'; // Estilos opcionales para esta página
 
 const Certificaciones = () => {
-  // Array de cursos con nombre e imagen
   const cursos = [
-    { id: 1, title: 'Curso 1', imgSrc: '/pdfs/IABigData.jpg', description: 'Descripción del curso 1' },
+    { id: 1, title: 'Curso 1', imgSrc: '/pdfs/p1.jpg', description: 'Descripción del curso 1' },
     { id: 2, title: 'Inteligencia Artificial y Big Data al Servicio de los Procesos de Selección de Personal', imgSrc: '/pdfs/IABigData.jpg', description: 'Certificado con el número de identificación: EC29160/15680' },
     { id: 3, title: 'Introducción al Desarrollo de Aplicaciones Web', imgSrc: '/pdfs/AppWeb.jpg', description: 'Certificado con el número de identificación: EC29160/16109' },
     { id: 4, title: 'Curso: Diagnóstico por Imagen', imgSrc: '/pdfs/DiagnosticoPorImagen.jpg', description: 'Certificado con el número de identificación: 1102021041669' },
@@ -12,22 +11,23 @@ const Certificaciones = () => {
   ];
   
   return (
-    <div className="certificaciones-container">
-      <header className="certificaciones-header">
-        <h1>Certificaciones</h1>
-      </header>
-      <div className="certificaciones-grid">
-        {cursos.map((curso) => (
-          <div className="certificacion-card" key={curso.id}>
-            <img src={curso.imgSrc} alt={curso.title} className="certificacion-image" />
-            <h2 className="certificacion-title">{curso.title}</h2>
-            <p className="certificacion-description">{curso.description}</p>
-          </div>
-        ))}
+    <div className="content">
+      <div className="certificaciones-container">
+        <header className="certificaciones-header">
+          <h1>Certificaciones</h1>
+        </header>
+        <div className="certificaciones-grid">
+          {cursos.map((curso) => (
+            <div className="certificacion-card" key={curso.id}>
+              <img src={curso.imgSrc} alt={curso.title} className="certificacion-image" />
+              <h2 className="certificacion-title">{curso.title}</h2>
+              <p className="certificacion-description">{curso.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default Certificaciones;
-
