f0ea13e5c4076ab930fc2f7b835009e4d8efc02d
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-29 16:21:24 +0200

    Update Certificaciones.jsx

--- src/pages/Certificaciones.jsx
+++ src/pages/Certificaciones.jsx
@@ -3,17 +3,10 @@ import './Certificaciones.css';
 
 const Certificaciones = () => {
   const certifications = [
-    { id: 1, title: 'Certificación 1', description: 'Descripción breve de la certificación 1.', imgSrc: '/pdfs/c1.jpg', pdfSrc: '/pdfs/certificacion1.pdf' },
-    { id: 2, title: 'Certificación 2', description: 'Descripción breve de la certificación 2.', imgSrc: '/pdfs/c2.jpg', pdfSrc: '/pdfs/certificacion2.pdf' },
-    { id: 3, title: 'Certificación 3', description: 'Descripción breve de la certificación 3.', imgSrc: '/pdfs/c3.jpg', pdfSrc: '/pdfs/certificacion3.pdf' },
-    { id: 4, title: 'Certificación 4', description: 'Descripción breve de la certificación 4.', imgSrc: '/pdfs/c4.jpg', pdfSrc: '/pdfs/certificacion4.pdf' },
-    { id: 5, title: 'Certificación 5', description: 'Descripción breve de la certificación 5.', imgSrc: '/pdfs/c5.jpg', pdfSrc: '/pdfs/certificacion5.pdf' },
-    { id: 6, title: 'Certificación 6', description: 'Descripción breve de la certificación 6.', imgSrc: '/pdfs/c6.jpg', pdfSrc: '/pdfs/certificacion6.pdf' },
+    { id: 1, title: 'Certificación 1', description: 'Descripción breve de la certificación 1.', imgSrc: '/pdfs/IABigData.jpg' },
+    { id: 2, title: 'Certificación 2', description: 'Descripción breve de la certificación 2.', imgSrc: '/pdfs/DiagnosticoPorImagen.jpg' },
+    { id: 3, title: 'Certificación 3', description: 'Descripción breve de la certificación 3.', imgSrc: '/pdfs/NuevasTecnicas.jpg' },
   ];
-
-  const handleOpenPdf = (pdfSrc) => {
-    window.open(pdfSrc, '_blank'); // Abre el PDF en una nueva pestaña
-  };
 
   return (
     <div className="certificaciones-container">
@@ -29,9 +22,6 @@ const Certificaciones = () => {
               <p className="cert-description">{cert.description}</p>
             </div>
             <img src={cert.imgSrc} alt={`Certificación ${cert.id}`} className="cert-image" />
-            <button className="cert-button" onClick={() => handleOpenPdf(cert.pdfSrc)}>
-              Ver PDF
-            </button>
           </div>
         ))}
       </div>
