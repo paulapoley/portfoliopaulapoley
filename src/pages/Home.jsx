3ee9fe5aad8cf7c51f6fc24c8a4377b72f74f0bb
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-08-31 20:00:17 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,14 @@ const Home = () => {
     },
     { 
       id: 2, 
+      title: 'Computación en la nube con amazon web services (AWS).', 
+      description: 'Uso de Amazon Web Services (AWS) para implementar y gestionar instancias EC2, configurar balanceadores de carga, y asignar direcciones IP fijas. El objetivo es familiarizarse con la infraestructura en la nube y aprender a manejar diferentes servicios y configuraciones dentro de AWS.', 
+      imgSrc: '/pdfs/AWS.jpg', 
+      pdfSrc: '/pdfs/Computacion-nube-AWS-PPC.pdf',
+      topics: ['AWS', 'EC2', 'ubuntu-server', 'cloudcomputing']
+    },
+    { 
+      id: 3, 
       title: 'BYODSEC: Implementación de BYOD en una Entidad Hospitalaria con VPN Road Warrior TLS', 
       description: 'Implementación de una arquitectura segura cliente-servidor en Java utilizando TLS para asegurar las comunicaciones. Se configuraron KeyStores, se desarrollaron sockets TLS y se validó el rendimiento para manejar hasta 300 conexiones concurrentes, garantizando autenticación y cifrado de datos.', 
       imgSrc: '/pdfs/p3.jpg', 
@@ -20,7 +28,7 @@ const Home = () => {
       topics: ['Java', 'TLS', 'VPN', 'BYOD', 'seguridad']
     },
     { 
-      id: 3, 
+      id: 4, 
       title: 'Digitalización de la autenticación y el control de acceso para servicio de salud de comunidad autónoma', 
       description: 'Implementación y gestión de certificados digitales para asegurar comunicaciones en servicios de salud. Incluye instalación y verificación de certificados, firma y validación de documentos, y automatización de políticas de control de acceso.', 
       imgSrc: '/pdfs/p2.jpg', 
@@ -28,7 +36,7 @@ const Home = () => {
       topics: ['certificados digitales', 'seguridad', 'automatización', 'control de acceso']
     },
     { 
-      id: 4, 
+      id: 5, 
       title: 'Análisis de datos con Weka, optimización y evaluación de modelos de aprendizaje automático.', 
       description: 'Implementación y ajuste de modelos de machine learning en Weka, incluyendo Bagging y agrupamiento con SimpleKMeans y EM. Optimización de parámetros y evaluación de rendimiento para mejorar la precisión y crear clasificadores personalizados.', 
       imgSrc: '/pdfs/p1.jpg', 
