18dc3b09a291b4e3a7d77fce88fb5cec45925b6b
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-09-02 14:03:27 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,14 @@ const Home = () => {
     },
     { 
       id: 2, 
+      title: 'App HUA+', 
+      description: 'La aplicación móvil desarrollada en Android Studio utilizando Kotlin, HUA+, está enfocada en la gestión y provisión de información sobre la Hemorragia Uterina Anormal (HUA), dirigida tanto a pacientes como a profesionales médicos.', 
+      imgSrc: '/pdfs/app-hua.png', 
+      pdfSrc: '/pdfs/APP-HUA.pdf',
+      topics: ['BPMN', 'Kotlin-Android Studio', 'Firebase-Authentication']
+    },
+    { 
+      id: 3, 
       title: 'Computación en la Nube con Amazon Web Services (AWS).', 
       description: 'Uso de Amazon Web Services (AWS) para implementar y gestionar instancias EC2, configurar balanceadores de carga, y asignar direcciones IP fijas. El objetivo es familiarizarse con la infraestructura en la nube y aprender a manejar diferentes servicios y configuraciones dentro de AWS.', 
       imgSrc: '/pdfs/AWS.jpg', 
@@ -20,7 +28,7 @@ const Home = () => {
       topics: ['AWS', 'EC2', 'Ubuntu Server', 'Cloudcomputing']
     },
     { 
-      id: 3, 
+      id: 4, 
       title: 'BYODSEC: Implementación de BYOD en una Entidad Hospitalaria con VPN Road Warrior TLS', 
       description: 'Implementación de una arquitectura segura cliente-servidor en Java utilizando TLS para asegurar las comunicaciones. Se configuraron KeyStores, se desarrollaron sockets TLS y se validó el rendimiento para manejar hasta 300 conexiones concurrentes, garantizando autenticación y cifrado de datos.', 
       imgSrc: '/pdfs/p3.jpg', 
@@ -28,7 +36,7 @@ const Home = () => {
       topics: ['Java', 'TLS', 'VPN', 'BYOD', 'Seguridad']
     },
     { 
-      id: 4, 
+      id: 5, 
       title: 'Digitalización de la Autenticación y el Control de Acceso para Servicio de Salud de Comunidad Autónoma', 
       description: 'Implementación y gestión de certificados digitales para asegurar comunicaciones en servicios de salud. Incluye instalación y verificación de certificados, firma y validación de documentos, y automatización de políticas de control de acceso.', 
       imgSrc: '/pdfs/p2.jpg', 
@@ -36,7 +44,7 @@ const Home = () => {
       topics: ['Certificados Digitales', 'Seguridad', 'Automatización', 'Control de Acceso']
     },
     { 
-      id: 5, 
+      id: 6, 
       title: 'Análisis de Datos con Weka, Optimización y Evaluación de Modelos de Aprendizaje Automático.', 
       description: 'Implementación y ajuste de modelos de machine learning en Weka, incluyendo Bagging y agrupamiento con SimpleKMeans y EM. Optimización de parámetros y evaluación de rendimiento para mejorar la precisión y crear clasificadores personalizados.', 
       imgSrc: '/pdfs/p1.jpg', 
@@ -44,7 +52,7 @@ const Home = () => {
       topics: ['Weka', 'Machine Learning', 'Optimización', 'Clasificadores']
     },
     { 
-      id: 6, 
+      id: 7, 
       title: 'Bone Marrow Transplant: Children Data Set', 
       description: 'Análisis predictivo de trasplante de médula ósea en pacientes pediátricos utilizando modelos de regresión logística y random forest. Incluye análisis y visualizaciones de datos clínicos para predecir la recaída post-trasplante.', 
       imgSrc: '/pdfs/bone.png', 
@@ -52,7 +60,7 @@ const Home = () => {
       topics: ['Data Science', 'Machine Learning', 'R', 'Random Forest']
     },
     { 
-      id: 7, 
+      id: 8, 
       title: 'Computación Paralela con Computadores de Memoria Distribuida usando MPI', 
       description: 'Se exploraron y aplicaron técnicas de computación paralela utilizando MPI en sistemas de memoria distribuida. Se modificaron y optimizaron programas para medir tiempos de ejecución, realizar estimaciones numéricas y analizar el rendimiento en diferentes configuraciones de procesos. La práctica incluyó ejercicios que abarcaron desde la modificación básica de programas hasta la ejecución paralela en múltiples PCs.', 
       imgSrc: '/pdfs/MPI.jpg', 
