e6b4518bbbdd33d74555f4b0632c9f1b39bd2f30
Paula Poley <99399775+paulapoley@users.noreply.github.com>
2024-09-22 12:27:35 +0200

    Update Certificaciones.jsx

--- src/pages/Certificaciones.jsx
+++ src/pages/Certificaciones.jsx
@@ -3,7 +3,7 @@ import './Certificaciones.css'; // Asegúrate de que este archivo esté correcta
 
 const Certificaciones = () => {
   const certifications = [
-    { id: 1, title: 'Programa de Inteligencia Artificial', description: 'Junio 2024 - Agosto 2024\n SkillUp Online en colaboración con IBM SkillsBuild. Verify: https://www.credly.com/go/00bpHpx3', imgSrc: '/pdfs/IA curso.jpg' },
+    { id: 1, title: 'Programa de Inteligencia Artificial', description: 'Junio 2024 - Agosto 2024\n SkillUp Online - IBM SkillsBuild. Verify: https://www.credly.com/go/00bpHpx3', imgSrc: '/pdfs/IA curso.jpg' },
     { id: 2, title: 'Inteligencia Artificial y Big Data al Servicio de los Procesos de Selección de Personal', description: '08/03/2023\nInstituto Andaluz de la Juventud, Junta de Andalucía\nCertificado con el número de identificación: EC29160/15680', imgSrc: '/pdfs/IABigData.jpg'},
     { id: 3, title: 'Introducción al Desarrollo de Aplicaciones Web', description: '01/03/2023\nInstituto Andaluz de la Juventud, Junta de Andalucía\nCertificado con el número de identificación: EC29160/16109', imgSrc: '/pdfs/AppWeb.jpg' },
     { id: 4, title: 'Curso: Diagnóstico por Imagen', description: '24/05/2021\nUniversidad Antonio de Nebrija\nCertificado con el número de identificación: 1102021041669', imgSrc: '/pdfs/DiagnosticoPorImagen.jpg'},
